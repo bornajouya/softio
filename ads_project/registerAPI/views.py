93f6618b1058c2873f0ea547d2be235400240cb5
Borna <youngbjouya@gmail.com>
2021-05-06 17:16:08 +0430

    register changed

--- ads_project/registerAPI/views.py
+++ ads_project/registerAPI/views.py
@@ -5,12 +5,24 @@ from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 
+
+def login(request):
+    form = CreateUserForm()
+    context = {'form': form}
+
+    # if request.method == 'POST':
+    #     form = CreateUserForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+
+    return render(request, 'login.html', context=context)
+
 # @never_cache
 def register(request):
     form = CreateUserForm()
     context = {'form': form}
 
-    if request.method == 'post':
+    if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
