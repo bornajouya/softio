8b959720ba14c8de3baba95fd721584ae8a786c7
Borna <youngbjouya@gmail.com>
2021-05-08 19:14:45 +0430

    register validate bug fixed

--- ads_project/registerAPI/urls.py
+++ ads_project/registerAPI/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('signup/', views.signup, name='signup'),
+    path(r'^login/$', views.login, {'template_name': 'login.html'}, name='login'),
+
     path('login-member/', views.login_member),
 
 ]
