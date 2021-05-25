6d713f68e71fb21ffe580f6665754337cd80462d
Borna <youngbjouya@gmail.com>
2021-05-25 20:24:53 +0430

    chat app

--- ads_project/adsAPI/views.py
+++ ads_project/adsAPI/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+import requests
+from django.views.decorators.cache import never_cache
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.http import *
+from django.shortcuts import redirect
+from django.template import RequestContext
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+import AdvancedHTMLParser
 
-# Create your views here.
-from .models import Ads
+
+def detail_view(request,id):
+
+
+    ads_ins = list(Ads.objects.values().filter(id=id))[0]
+
+    context ={"ads_ins": ads_ins}
+    print(ads_ins)
+    print(type(ads_ins))
+
+
+    print(id)
+    return render(request, 'detail.html',context=context)
