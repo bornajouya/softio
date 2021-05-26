99156b00d11586fb7da0f5628d9ec43d6ebc8915
Borna <youngbjouya@gmail.com>
2021-05-26 15:49:57 +0430

    add chat models and templates

---
+++ ads_project/chatAPI/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from registerAPI.models import User
+from datetime import datetime
+from adsAPI.models import Ads
+# Create your models here.
+class chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
+    time = models.DateTimeField(default=datetime.now())
+    ads = models.ForeignKey(Ads, on_delete=models.CASCADE)
+    isfirst = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.text)
