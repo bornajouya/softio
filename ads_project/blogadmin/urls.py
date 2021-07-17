3b081b9dceb3dbb7edcbeac40a9b0aa994b13770
orna <youngbjouya@gmail.com>
2021-07-17 18:48:21 +0430

    last debug : addcontent finished

--- ads_project/blogadmin/urls.py
+++ ads_project/blogadmin/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
    path('', views.add, name='add'),
+   path('addcontent/', views.addcontent, name='addcontent'),
 ]
--- ads_project/blogadmin/views.py
+++ ads_project/blogadmin/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import render
 from adsAPI.models import Ads
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
 
+from django.contrib import messages
 
 # Create your views here.
 def add(request):
-    # ads_ins = list(Ads.objects.values().filter(id=id))[0]
 
-    # context = {
-    #     "ads_ins": ads_ins,
-    #     "this_chat": Chat_View(request, id)
-    # }
-    # print('this is un updated context %s' % context)
-    # # chat_context= Chat_View(request,id)
-    # # print('this is chat %s' % chat_context)
-    # # context = context.update(chat_context)
-    # print('this is %s context' % context)
 
     context = {"ads_list": list(Ads.objects.all().filter(user=request.user).values())}
     # Add your context here
 
     return render(request, 'add.html', context=context)
+
+@csrf_exempt
+def addcontent(request):
+    # logout(request)
+    if request.POST:
+        ads_name = request.POST.get('ads_name',False)
+        description = request.POST.get('description',False)
+        print(request)
+        print(request.POST['ads_name'])
+        print(request.POST['description'])
+        # description = request.POST['description']
+
+        # try:
+        ads_ins = Ads(title=str(ads_name), description=description,user=request.user)
+        ads_ins.save()
+        return redirect("index")
+
+        # except Exception as ex :
+        #     messages.info(request, 'username or password is incorrect')
+
+    return redirect("index")
+
+
